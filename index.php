fdb58a56f58c7edad80e32f676e6ae7f9f9e5a8a
kathagui15 <kathagui15@gmail.com>
2021-10-17 16:20:18 -0500

    cree el index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+­­<?php
+// asigancion
+
+
+$name = "yamy"; //string
+$lastmane = "rueda"; //string
+$age = 31; //number
+
+$jobis = [
+    "monday" => "read",
+    "sunday" => "play the guitar"
+];
+
+    
+?>
